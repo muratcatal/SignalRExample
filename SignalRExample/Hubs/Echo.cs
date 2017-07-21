8320d8a0389205afddddfef8964d4cae98cad59a
Murat ÇATAL <muratcatal@outlook.com>
2017-07-21 14:59:48 +0300

    Add project files.

---
+++ SignalRExample/Hubs/Echo.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRExample.Hubs
+{
+    // All signalr names (hub name, function names) will be processed as pascal-case
+    // However, we can also override it by using attributes as shown below
+    [HubName("echo")]
+    public class Echo : Hub
+    {
+        // our signalr server
+        // only public functions can be accessed by clients
+        [HubMethodName("say")]
+        public void Say(string message)
+        {
+            // call back for client (calling a function defined in javascript from server side)
+            Clients.All.replied($"{message}");
+        }
+    }
+}
---
+++ SignalRExample/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRExample.Startup))]
+
+namespace SignalRExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Do not forget to map your signalr
+            app.MapSignalR();
+        }
+    }
+}
